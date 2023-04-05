ad4bfa2c857e7a7a123733ac3790642ab545f955
dnwn-9001 <j10511@hanmail.net>
2023-04-05 17:40:47 +0900

    img 버튼 클릭시 모달창 on/off 기능 추가

--- build/main.js
+++ build/main.js
@@ -1,3 +1,18 @@
 "use strict";
-console.log("hello world");
+function turnOnPopup() {
+    var popupBackground = document.querySelector(".popup__background");
+    popupBackground === null || popupBackground === void 0 ? void 0 : popupBackground.classList.remove("display_none");
+}
+function turnOffPopup() {
+    var popupBackground = document.querySelector(".popup__background");
+    popupBackground === null || popupBackground === void 0 ? void 0 : popupBackground.classList.add("display_none");
+}
+var imgBtn = document.querySelector("#img_btn");
+var closeBtn = document.querySelector(".popup__close__btn");
+imgBtn === null || imgBtn === void 0 ? void 0 : imgBtn.addEventListener("click", function () {
+    turnOnPopup();
+});
+closeBtn === null || closeBtn === void 0 ? void 0 : closeBtn.addEventListener("click", function () {
+    turnOffPopup();
+});
 //# sourceMappingURL=main.js.map
--- src/main.ts
+++ src/main.ts
@@ -1,2 +1,22 @@
 "use strict";
-console.log("hello world");
+
+function turnOnPopup() {
+  const popupBackground = document.querySelector(".popup__background");
+  popupBackground?.classList.remove("display_none");
+}
+
+function turnOffPopup() {
+  const popupBackground = document.querySelector(".popup__background");
+  popupBackground?.classList.add("display_none");
+}
+
+const imgBtn = document.querySelector("#img_btn");
+const closeBtn = document.querySelector(".popup__close__btn");
+
+imgBtn?.addEventListener("click", () => {
+  turnOnPopup();
+});
+
+closeBtn?.addEventListener("click", () => {
+  turnOffPopup();
+});
