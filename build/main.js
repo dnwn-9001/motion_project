53d37f7eac17d3dab1143c824b1a17cc79aa7fec
dnwn-9001 <j10511@hanmail.net>
2023-04-07 17:14:23 +0900

    네개 버튼 클릭시 팝업(클래스로 기능 정의)

--- build/main.js
+++ build/main.js
@@ -1,18 +1,55 @@
 "use strict";
-function turnOnPopup() {
-    var popupBackground = document.querySelector(".popup__background");
-    popupBackground === null || popupBackground === void 0 ? void 0 : popupBackground.classList.remove("display_none");
-}
-function turnOffPopup() {
-    var popupBackground = document.querySelector(".popup__background");
-    popupBackground === null || popupBackground === void 0 ? void 0 : popupBackground.classList.add("display_none");
-}
-var imgBtn = document.querySelector("#img_btn");
-var closeBtn = document.querySelector(".popup__close__btn");
-imgBtn === null || imgBtn === void 0 ? void 0 : imgBtn.addEventListener("click", function () {
-    turnOnPopup();
-});
-closeBtn === null || closeBtn === void 0 ? void 0 : closeBtn.addEventListener("click", function () {
-    turnOffPopup();
-});
+var Popup = /** @class */ (function () {
+    function Popup(classOrIdName) {
+        this.btnElement = document.querySelector(classOrIdName);
+    }
+    Popup.prototype.clickTurnOnPopupBtn = function () {
+        var _this = this;
+        var _a;
+        (_a = this.btnElement) === null || _a === void 0 ? void 0 : _a.addEventListener("click", function () {
+            _this.turnOnPopup();
+        });
+    };
+    Popup.prototype.clickTurnOffPopupBtn = function () {
+        var _this = this;
+        var _a;
+        (_a = this.btnElement) === null || _a === void 0 ? void 0 : _a.addEventListener("click", function () {
+            _this.turnOffPopup();
+        });
+    };
+    Popup.prototype.turnOnPopup = function () {
+        // if (this.btnElement && (this.btnElement.id === "note_btn" || "task_btn")) {
+        //   console.log(this.btnElement.id);
+        //   this.changeNoteAndTaskContents();
+        // }
+        var popupBackground = document.querySelector(".popup__background");
+        popupBackground === null || popupBackground === void 0 ? void 0 : popupBackground.classList.remove("display_none");
+    };
+    Popup.prototype.turnOffPopup = function () {
+        var popupBackground = document.querySelector(".popup__background");
+        popupBackground === null || popupBackground === void 0 ? void 0 : popupBackground.classList.add("display_none");
+    };
+    Popup.prototype.changeNoteAndTaskContents = function () {
+        var inputLabel = document.querySelector("#input_label");
+        var secondInput = document.querySelector(".popup__url__title");
+        if (inputLabel && secondInput) {
+            inputLabel.innerHTML = "";
+            secondInput.remove();
+            inputLabel.innerHTML += "Body <br/>";
+            secondInput.innerHTML += "<textarea></textarea>";
+            secondInput.classList.add("popup__url__title");
+        }
+    };
+    return Popup;
+}());
+var imgPopup = new Popup("#img_btn");
+var videoPopup = new Popup("#video_btn");
+var notePopup = new Popup("#note_btn");
+var taskPopup = new Popup("#task_btn");
+var popupCloseBtn = new Popup(".popup__close__btn");
+imgPopup.clickTurnOnPopupBtn();
+videoPopup.clickTurnOnPopupBtn();
+notePopup.clickTurnOnPopupBtn();
+taskPopup.clickTurnOnPopupBtn();
+popupCloseBtn.clickTurnOffPopupBtn();
 //# sourceMappingURL=main.js.map
--- src/main.ts
+++ src/main.ts
@@ -1,22 +1,60 @@
 "use strict";
 
-function turnOnPopup() {
-  const popupBackground = document.querySelector(".popup__background");
-  popupBackground?.classList.remove("display_none");
+class Popup {
+  private btnElement: Element | null;
+
+  constructor(classOrIdName: string) {
+    this.btnElement = document.querySelector(classOrIdName);
+  }
+
+  clickTurnOnPopupBtn() {
+    this.btnElement?.addEventListener("click", () => {
+      this.turnOnPopup();
+    });
+  }
+
+  clickTurnOffPopupBtn() {
+    this.btnElement?.addEventListener("click", () => {
+      this.turnOffPopup();
+    });
+  }
+
+  turnOnPopup() {
+    // if (this.btnElement && (this.btnElement.id === "note_btn" || "task_btn")) {
+    //   console.log(this.btnElement.id);
+
+    //   this.changeNoteAndTaskContents();
+    // }
+    const popupBackground = document.querySelector(".popup__background");
+    popupBackground?.classList.remove("display_none");
+  }
+
+  turnOffPopup() {
+    const popupBackground = document.querySelector(".popup__background");
+    popupBackground?.classList.add("display_none");
+  }
+
+  changeNoteAndTaskContents() {
+    const inputLabel = document.querySelector("#input_label");
+    const secondInput = document.querySelector(".popup__url__title");
+    if (inputLabel && secondInput) {
+      inputLabel.innerHTML = "";
+      secondInput.remove();
+      inputLabel.innerHTML += "Body <br/>";
+      secondInput.innerHTML += "<textarea></textarea>";
+      secondInput.classList.add("popup__url__title");
+    }
+  }
 }
 
-function turnOffPopup() {
-  const popupBackground = document.querySelector(".popup__background");
-  popupBackground?.classList.add("display_none");
-}
+const imgPopup = new Popup("#img_btn");
+const videoPopup = new Popup("#video_btn");
+const notePopup = new Popup("#note_btn");
+const taskPopup = new Popup("#task_btn");
+const popupCloseBtn = new Popup(".popup__close__btn");
 
-const imgBtn = document.querySelector("#img_btn");
-const closeBtn = document.querySelector(".popup__close__btn");
-
-imgBtn?.addEventListener("click", () => {
-  turnOnPopup();
-});
-
-closeBtn?.addEventListener("click", () => {
-  turnOffPopup();
-});
+imgPopup.clickTurnOnPopupBtn();
+videoPopup.clickTurnOnPopupBtn();
+notePopup.clickTurnOnPopupBtn();
+taskPopup.clickTurnOnPopupBtn();
+popupCloseBtn.clickTurnOffPopupBtn();
